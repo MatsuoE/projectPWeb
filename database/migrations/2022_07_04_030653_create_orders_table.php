b04497766950183748b84e8e20332b9699ccd23e
Auliya Rakhman <96972918+MatsuoE@users.noreply.github.com>
2022-07-04 12:46:07 +0700

    update transaksi

---
+++ database/migrations/2022_07_04_030653_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cart_id')->unsigned();
+            $table->string('nama_penerima');
+            $table->string('number');
+            $table->text('alamat');
+            $table->foreign('cart_id')->references('id')->on('cart');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
